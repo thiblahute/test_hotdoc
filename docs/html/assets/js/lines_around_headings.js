d2f30d632955d645dd91ebc3b5ba752802702089
Thibault Saunier <thibault.saunier@osg.samsung.com>
2017-04-02 11:27:55 -0300

    Add documentation to be used with gh pages

---
+++ docs/html/assets/js/lines_around_headings.js
@@ -0,0 +1,18 @@
+function add_section_headings_class() {
+	var headings = $("h1");
+
+	if (headings.length < 2) {
+		headings.attr("data-toc-skip", "true");
+		headings = $("h2:not(.symbol_section)").add("h2:not(.symbols_section)")
+	}
+    console.log(headings)
+
+	if (headings.length > 1)
+		headings.addClass("section_headings");
+	else
+		headings.attr("data-toc-skip", "true");
+}
+
+$(document).ready(function() {
+	add_section_headings_class();
+});
